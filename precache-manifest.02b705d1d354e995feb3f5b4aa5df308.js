e9eb74c20777c345ae28c33ade3cf4512263bf56
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2019-10-10 22:20:08 +0530

    Updates

---
+++ precache-manifest.02b705d1d354e995feb3f5b4aa5df308.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c26489be108d54fd485fe6c9ee5407f",
+    "url": "/portfolio-v4/index.html"
+  },
+  {
+    "revision": "4c780277ee0ff88906a9",
+    "url": "/portfolio-v4/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4e6c8d5a5e148945c707",
+    "url": "/portfolio-v4/static/css/main.e65736c7.chunk.css"
+  },
+  {
+    "revision": "4c780277ee0ff88906a9",
+    "url": "/portfolio-v4/static/js/2.e73c9996.chunk.js"
+  },
+  {
+    "revision": "4e6c8d5a5e148945c707",
+    "url": "/portfolio-v4/static/js/main.41d709fb.chunk.js"
+  },
+  {
+    "revision": "eca4e86c2ff5bb81210f",
+    "url": "/portfolio-v4/static/js/runtime-main.89a85752.js"
+  },
+  {
+    "revision": "1aa927a19226ea7efde0884d8f0c976a",
+    "url": "/portfolio-v4/static/media/avatar-1.1aa927a1.png"
+  },
+  {
+    "revision": "0f275b45066f152f8abe90fb523e0b10",
+    "url": "/portfolio-v4/static/media/budget.0f275b45.png"
+  },
+  {
+    "revision": "b17afdee3683c9ce2b8caf0376115549",
+    "url": "/portfolio-v4/static/media/profile.b17afdee.jpg"
+  },
+  {
+    "revision": "1f5de0c4ecf0413c4237ab5020929806",
+    "url": "/portfolio-v4/static/media/robofriends.1f5de0c4.png"
+  },
+  {
+    "revision": "5b63644acc549eeb4ae7dc088c2af813",
+    "url": "/portfolio-v4/static/media/smartbrain.5b63644a.png"
+  }
+]);
